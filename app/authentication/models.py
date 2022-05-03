8c56423006114c41e3c7b03cea949f6209b96060
Prabu Mahendran <prabumahendran@Prabus-MacBook-Pro.local>
2022-05-03 07:48:06 -0400

    Change in models

--- app/authentication/models.py
+++ app/authentication/models.py
@@ -1,7 +1,9 @@
 from app import db
+from app import app
 from flask_jwt import JWT, jwt_required, current_identity
 from werkzeug.security import safe_str_cmp
-
+import datetime
+import jwt
 
 
 class Users(db.Model):
@@ -26,3 +28,22 @@ class Users(db.Model):
 
     def __repr__(self):
         return str(self.username)
+
+    def encode_auth_token(self, user_id):
+        """
+        Generates the Auth Token
+        :return: string
+        """
+        try:
+            payload = {
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
+                'iat': datetime.datetime.utcnow(),
+                'sub': user_id
+            }
+            return jwt.encode(
+                payload,
+                app.config.get('SECRET_KEY'),
+                algorithm='HS256'
+            )
+        except Exception as e:
+            return e

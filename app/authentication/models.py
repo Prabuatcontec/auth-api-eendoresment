bfae95e1f2dc1ee98da0832918d330bebfeaeaca
Prabu Mahendran <prabumahendran@Prabus-MacBook-Pro.local>
2022-05-04 18:47:07 -0400

    Password validation

--- app/authentication/models.py
+++ app/authentication/models.py
@@ -8,6 +8,7 @@ from jose import jws
 from cryptography.hazmat.primitives import serialization as crypto_serialization
 from cryptography.hazmat.backends import default_backend
 from dotenv import load_dotenv
+import bcrypt
 
 
 class Users(db.Model):
@@ -16,6 +17,7 @@ class Users(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100), unique=False)
 
     def __init__(self, **kwargs):
         for property, value in kwargs.items():
@@ -34,31 +36,33 @@ class Users(db.Model):
     def __repr__(self):
         return str(self.username)
 
-    def encode_auth_token(self, user_id, username):
+    def encode_auth_token(self, user_id, username, password, _password):
         """
         Generates the Auth Token
         :return: string
         """ 
         try:
-            private_key_pem = str(app.config.get('JWT_PRIVATE_KEY'))
-            print(private_key_pem)
-            with open(private_key_pem, "rb") as key_file:
-                pem_bytes = key_file.read()
-                passphrase = bytes(app.config.get('JWT_PASE_PHRASE'),'utf-8')
-                private_key = crypto_serialization.load_pem_private_key(pem_bytes, password=passphrase, backend=default_backend())
+            username = username
+            _password = bytes(_password,'utf-8')
+            password = bytes(password,'utf-8')
 
-            payload = {
-                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=3000),
-                'iat': datetime.datetime.utcnow(),
-                'username': username,
-                'id': user_id 
-            }
-            return  jwt.encode(payload, private_key, algorithm='RS256')
-            return jwt.encode(
-                payload,
-                app.config.get('SECRET_KEY'),
-                algorithm='RS256'
-            )
+            if bcrypt.checkpw(_password, password):
+                private_key_pem = str(app.config.get('JWT_PRIVATE_KEY'))
+                with open(private_key_pem, "rb") as key_file:
+                    pem_bytes = key_file.read()
+                    passphrase = bytes(app.config.get('JWT_PASE_PHRASE'),'utf-8')
+                    private_key = crypto_serialization.load_pem_private_key(pem_bytes, password=passphrase, backend=default_backend())
+
+                payload = {
+                    'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=3000),
+                    'iat': datetime.datetime.utcnow(),
+                    'username': username,
+                    'id': user_id 
+                }
+                return  jwt.encode(payload, private_key, algorithm='RS256')
+            else:
+                return False
+            
         except Exception as e:
             return e
 
--- app/authentication/routes.py
+++ app/authentication/routes.py
@@ -1,8 +1,9 @@
 from time import sleep
-from flask import Blueprint, request, Response, session, Flask, jsonify
+from flask import Blueprint, request, Response, session, Flask, jsonify, abort
+from sqlalchemy import false
 from mysqlConnect import Connection
 from flask_api import status
-
+import bcrypt
 
 blueprint = Blueprint(
     'authentication_blueprint',
@@ -12,11 +13,42 @@ blueprint = Blueprint(
 
 from authentication.models import Users 
 
-@blueprint.route('/login')
+@blueprint.route('/credential')
+def auth():
+    username = "prabum1985@gmail.com"
+    passwd = b'Eendor@123'
+
+    # hashed = b'59ffcf24ec564c07795bd11918ee6cb91d9f67077d505f021ee96dc45b97dd2d1d50554e3639d3664ae68ed277d0c8a4bf7c'
+    # hashed = bcrypt.hashpw(passwd, salt)
+    # print("hash")
+    # print(hashed)
+ 
+    hashed = b'$2y$13$Keux2pQHAwiuVeerXQGBcuuk.ea9C9K0jl6xhlkMFRUenyVnrAeu.'
+    
+    if bcrypt.checkpw(passwd, hashed):
+        return "match"
+    else:
+        return "does not match"
+        
+@blueprint.route('/login', methods = ['POST'])
 def authentication():
-    user = Users().encode_auth_token(123, 'brian.crehan@azmoves.com')
-    responseBody = { "token": user }
-    return jsonify(responseBody), status.HTTP_200_OK
+    username = request.form['_username'] 
+    password = request.form['_password']
+    validation = False
+    if username is None or password is None:
+        validation = True
+    user = Users.query.filter_by(username = username).first()
+    if user is None:
+        validation = True
+    if validation == False:
+        token = Users().encode_auth_token(user.id, user.username, user.password, password)
+        if token == False:
+            validation = True
+        else: 
+            return jsonify({ "token": token }), status.HTTP_200_OK
+    if validation == True:
+        return jsonify({ "code": status.HTTP_400_BAD_REQUEST, "message": "Bad credentials" }), status.HTTP_400_BAD_REQUEST
+    
 
 @blueprint.route('/decode')
 def decode_token():
